7bfdc806e1825ad9af5c281b97d0b1cb53175f8a
Abheekrishna <abheekrishna17@gmail.com>
2023-05-14 10:45:52 +0530

    Created a bank app. There is a bug line 88

---
+++ app.js
@@ -0,0 +1,107 @@
+/* Dom
+createAccount
+deleteAccount
+nameOfPerson
+balance
+transactions
+deposit
+withdraw
+*/
+let accountCreated = false;
+let balance = 0;
+
+const createAccount = document.getElementById('createAccount');
+const deleteAccount = document.getElementById('deleteAccount');
+const nameOfPerson = document.getElementById('nameOfPerson');
+const transactions = document.getElementById('transactions');
+const balanceDiv = document.getElementById('balance')
+const depositBtn = document.getElementById('deposit');
+const withdrawDiv = document.getElementById('withdraw');
+
+
+const changeBalance = () => {
+    let balance = Number(document.getElementById('balance').innerText);
+    return balance;
+}
+
+const changeBalanceDepo = (amount) => {
+    let balance = Number(document.getElementById('balance').innerText);
+    balanceDiv.innerText = balance + Number(amount);
+}
+
+const changeBalanceWithdraw = (amount) => {
+    let balance = Number(document.getElementById('balance').innerText);
+    balanceDiv.innerText = balance - Number(amount);
+
+}
+
+const accountCreating = () => {
+    if (accountCreated) {
+        transactions.innerText = `You already created an account`;
+    } else if ( accountCreated == false) {
+        const username = prompt('What\'s your name?')
+        if (username.trim().length === 0) {
+            transactions.innerText = `You didn't enter your name.`;
+            return;
+        } else {
+        nameOfPerson.innerText = `${username}`;
+        balanceDiv.innerText = `${balance}`
+        transactions.innerText = `New created successfully.`
+        accountCreated = true;
+        }
+    }
+}
+
+const accountDeleting = () => {
+    accountCreated = false;
+    nameOfPerson.innerText = '';
+    balanceDiv.innerText = '';
+    transactions.innerText = 'Account deleted.'
+}
+
+const amountDeposit = () => {
+    const amount = prompt('How much you want to deposit?');
+    if (isNaN(amount)) {
+        transactions.innerText = `Invalid amount.`;
+    } else {
+    changeBalanceDepo(amount);
+    transactions.innerText = `Deposited: ₹${amount}`;
+    }
+}
+
+const amountWithraw = () => {
+    const amount = prompt('How much you want to withdraw?');
+    if (isNaN(amount)) {
+        transactions.innerText = `Invalid amount.`
+    } else {
+    if (amount - changeBalance() > 0) {
+        transactions.innerText = 'You can\'t withdraw more than your balance.'
+    } else {
+    changeBalanceWithdraw(amount);
+    transactions.innerText = `Withdrawn: ₹${amount}`
+    }
+}
+}
+
+
+
+const hasAccountCreated = () => {
+    if (accountCreated == true) {
+        deleteAccount.onclick = () => accountDeleting();
+        withdrawDiv.onclick = () => amountWithraw();
+        depositBtn.onclick = () => amountDeposit();
+    } else if (accountCreated == false) {
+        deleteAccount.onclick = () => {
+            transactions.innerText = 'You need to create one to delete.';
+        };
+        withdrawDiv.onclick = () => {
+            transactions.innerText = 'You need to create an account for proceeding.';
+        };
+        depositBtn.onclick = () => {
+            transactions.innerText = 'You need to create an account for proceeding.';
+        };
+    }
+}
+
+hasAccountCreated();
+createAccount.onclick = () => accountCreating();
